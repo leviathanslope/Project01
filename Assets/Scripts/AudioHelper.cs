9df156f5cde09bf8403da60f4f3ec38433b2b04f
Minh <minhanhduong33@gmail.com>
2022-09-01 23:16:43 -0500

    Added Ground

---
+++ Assets/Scripts/AudioHelper.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AudioHelper
+{
+    public static AudioSource PlayClip2D(AudioClip clip, float volume)
+    {
+        GameObject audioObject = new GameObject("Audio2D");
+        AudioSource audioSource = audioObject.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.volume = volume;
+        audioSource.Play();
+        Object.Destroy(audioObject, clip.length);
+        return audioSource;
+    }
+}
